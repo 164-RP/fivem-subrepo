c225fd118eda767730641ba7e58240e11a054474
164RP <labrp5m@gmail.com>
2024-06-30 17:03:53 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-export ORG_NAME="iLLeniumTest"
-export REPO_NAME="Resources"
-export REPO_PATH="$HOME/go/github.com"
+export ORG_NAME="164RP"
+export REPO_NAME="resources"
+export REPO_PATH="$HOME/FiveM"
 export RECIPE_PATH="$(pwd)/recipes/qbcore.yaml"
